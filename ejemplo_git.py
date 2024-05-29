55c574ce3bbf4449b6ec7be2ed8dcd8db1e0ba8a
johanbecerra <joh.becerram@duocuc.cl>
2024-05-29 09:22:42 -0400

    cree mi diccionario del ejercicio

--- ejemplo_git.py
+++ ejemplo_git.py
@@ -1,1 +1,7 @@
 #ejercicio diccionario de hoy: ususarios: 
+diccionario={
+    "identificador": 1,
+    "nombre": "marriott",
+    "direccion": "avenida bernardo o'higgins 1123",
+    "valor_avitacion": 100000
+}
